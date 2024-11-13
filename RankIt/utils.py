c98ffcdc0f8d822866648e59b2ab5bd8833bb0c8
Nathan Guimarães <nathanhguimaraes@gmail.com>
2024-11-13 13:04:43 -0300

    IIII

--- RankIt/utils.py
+++ RankIt/utils.py
@@ -1,11 +1,6 @@
 from pymongo import MongoClient
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-mongo_uri = os.getenv("MONGO_URI")
-client = MongoClient(mongo_uri)
+client = MongoClient()
 db = client['rankit_db']
 users_collection = db['users_collection']
 
